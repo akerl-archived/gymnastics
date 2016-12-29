c3dc5d7077341e18de6a20dc68631cecf17f3a7c
Les Aker <me@lesaker.org>
2016-12-29 16:40:38 -0500

    add averaging/sorting

--- scripts/math.rb
+++ scripts/math.rb
@@ -25,6 +25,8 @@ end
 ##
 # Helper class for accessing gymnasts
 class Gymnast
+  include Comparable
+
   attr_reader :name, :team, :scores, :wildcard
 
   def initialize(file)
@@ -45,7 +47,27 @@ class Gymnast
       true
     end
   end
+
+  def averages
+    @averages ||= EVENTS.map do |event|
+      data = @scores.send(event)
+      next [event, 0] if data.empty?
+      [event, data.compact.reduce(:+) / data.size]
+    end.to_h
+  end
+
+  def <=>(other)
+    return nil unless other.is_a? Gymnast
+    my_avgs = averages.values.sort
+    other_avgs = other.averages.values.sort
+    my_avgs.zip(other_avgs).each do |my_score, other_score|
+      res = my_score <=> other_score
+      return -1 * res unless res.zero?
+    end
+    0
+  end
 end
+
 
 def find(name)
   GYMNASTS.find { |x| x.name.match name }
@@ -59,20 +81,20 @@ GYMNASTS = Dir.glob(STATS_DIR + '/*/*').map { |file| Gymnast.new file }
 TEAMS = GYMNASTS.group_by(&:team)
 
 mine = (
-    # Folks who get 10s
-    GYMNASTS.select { |x| EVENTS.select { |y| x.scores.send(y).select { |z| z == 10 }.size > 0 }.size > 0 } + \
-    # Star freshmen
-    %w(mykaylaskinner maggienichols ameliahundley).map { |x| find x } + \
-    # Folks who compete great in 2+ events
-    GYMNASTS.select { |x| x.competes(0.8, 9.8).size > 1 } + \
-    # Folks who compete excellently in any event
-    GYMNASTS.select { |x| x.competes(0.7, 9.9).size > 0 } + \
-    # Folks who compete well in all 4 events
-    GYMNASTS.select { |x| x.competes(0.8, 9.5).size > 3 } + \
-    # Promising freshmen
-    %w(kennediedney maddiekarr kimtessen taylorhouchin cassidykellen racheldickson graceglenn wynterchilders missyreinstadtler samogden).map { |x| find x } + \
-    # Folks who always compete in 3 or more events
-    GYMNASTS.select { |x| x.competes(1.0, 9.6).size > 2 }
+  # Folks who get 10s
+  GYMNASTS.select { |x| EVENTS.select { |y| x.scores.send(y).select { |z| z == 10 }.size > 0 }.size > 0 }.sort + \
+  # Star freshmen
+  %w(mykaylaskinner maggienichols ameliahundley).map { |x| find x }.sort + \
+  # Folks who compete great in 2+ events
+  GYMNASTS.select { |x| x.competes(0.8, 9.8).size > 1 }.sort + \
+  # Folks who compete excellently in any event
+  GYMNASTS.select { |x| x.competes(0.7, 9.9).size > 0 }.sort + \
+  # Folks who compete well in all 4 events
+  GYMNASTS.select { |x| x.competes(0.8, 9.5).size > 3 }.sort + \
+  # Promising freshmen
+  %w(kennediedney maddiekarr kimtessen taylorhouchin cassidykellen racheldickson graceglenn wynterchilders missyreinstadtler samogden).map { |x| find x }.sort + \
+  # Folks who always compete in 3 or more events
+  GYMNASTS.select { |x| x.competes(1.0, 9.6).size > 2 }.sort
 )
 
 # Collect frequent leaders from teams not already represented
@@ -80,13 +102,13 @@ freq_unrep = TEAMS.reject { |k, v| mine.group_by(&:team).keys.include? k }.map {
 # Collect frequent leaders from teams with weak representation
 freq_lowrep = TEAMS.reject { |k, v| (mine.group_by(&:team)[k] || [1] * 4).size > 2 }.map { |k, v| [k, v.select { |x| x.competes(0.9, 9.5).size > 2 }] }.reject { |k, v| v.empty? }.to_h.values.flatten
 # Add above two collections to draft pool
-mine += freq_unrep + freq_lowrep
+mine += freq_unrep.sort + freq_lowrep.sort
 
 # Add gymnasts who got equal to or better than 9.9 at least twice on at least 2 events
-mine += GYMNASTS.select { |x| EVENTS.select { |y| x.scores.send(y).select { |z| (z || 0) >= 9.9 }.size > 1 }.size > 1 }
+mine += GYMNASTS.select { |x| EVENTS.select { |y| x.scores.send(y).select { |z| (z || 0) >= 9.9 }.size > 1 }.size > 1 }.sort
 
 # Add gymnasts who got better than 9.9 at least twice on at least 1 events
-mine += GYMNASTS.select { |x| EVENTS.select { |y| x.scores.send(y).select { |z| (z || 0) > 9.9 }.size > 1 }.size > 0 }
+mine += GYMNASTS.select { |x| EVENTS.select { |y| x.scores.send(y).select { |z| (z || 0) > 9.9 }.size > 1 }.size > 0 }.sort
 
 rand = Random.new(3)
 count = 18
