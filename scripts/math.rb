5533001ac69695e7c57da42ae2798da67c80d456
Les Aker <me@lesaker.org>
2016-12-29 02:08:06 -0500

    clean up scripts

--- scripts/math.rb
+++ scripts/math.rb
@@ -7,33 +7,57 @@ Dir.chdir File.expand_path(File.dirname(__FILE__))
 
 STATS_DIR = '../stats'.freeze
 
+EVENTS = [:vault, :bars, :beam, :floor].freeze
+
 ##
 # Helper class for accessing scores
 class Scores
   attr_reader :vault, :bars, :beam, :floor
   def initialize(scores)
     scores ||= {}
-    @vault = scores['vault']
-    @bars = scores['bars']
-    @beam = scores['beam']
-    @floor = scores['floor']
+    @vault = scores['vault'] || []
+    @bars = scores['bars'] || []
+    @beam = scores['beam'] || []
+    @floor = scores['floor'] || []
   end
 end
 
 ##
 # Helper class for accessing gymnasts
 class Gymnast
-  attr_reader :name, :team
+  attr_reader :name, :team, :scores
+
   def initialize(file)
     data = JSON.parse(File.read(file))
     @name = data['name']
     @team = data['team']
     @scores = Scores.new(data['score'])
   end
+
+  def competes(freq_min = 0.8, score_min = 9.0)
+    EVENTS.select do |event|
+      full = @scores.send(event)
+      next false if full.empty?
+      data = full.compact
+      next false unless data.size.to_f / full.size >= freq_min
+      next false unless data.reduce(:+) / data.size >= score_min
+      true
+    end
+  end
+end
+
+def find(name)
+  GYMNASTS.find { |x| x.name.match name }
+end
+
+def find_all(name)
+  GYMNASTS.select { |x| x.name.match name }
 end
 
 gymnasts = Dir.glob(STATS_DIR + '/*/*').map { |file| Gymnast.new file }
+GYMNASTS = gymnasts
 teams = gymnasts.group_by(&:team)
+TEAMS = teams
 
 # rubocop:disable Lint/Debugger
 g = gymnasts
--- scripts/stats.rb
+++ scripts/stats.rb
@@ -8,22 +8,30 @@ require 'json'
 
 Dir.chdir File.expand_path(File.dirname(__FILE__))
 
-BROWSER = Watir::Browser.new
 BASE_URL = 'http://www.roadtonationals.com/results/charts/ch_consistency_i.php'.freeze
 BASE_PERSON_URL = "#{BASE_URL}?yr=2016&z=0&gid=".freeze
 
 SCORE_START = /\A.*series: \[/m
 SCORE_STOP = /\s+\]\s+}\);\s+}\);\s+}\);\s+\z/m
 
+def browser
+  @browser ||= Watir::Browser.new
+end
+
 def load_page(id)
-  BROWSER.goto BASE_PERSON_URL + id
-  Nokogiri::HTML(BROWSER.html)
+  browser.goto BASE_PERSON_URL + id
+  Nokogiri::HTML(browser.html)
+end
+
+def load_file(file)
+  File.readlines('../data/' + file).map do |x|
+    x.chomp.split('---')
+  end
 end
 
 FileUtils.mkdir_p '../stats'
-people_ids = File.readlines('../data/found.txt').map do |x|
-  x.chomp.split('---')
-end
+people_ids = load_file 'found.txt'
+wildcards = load_file 'wildcards.txt'
 
 def extract_script(page)
   score_script = page.css('script').find do |y|
@@ -39,11 +47,15 @@ def parse_score(page)
   end.to_h
 end
 
+def get_stat_path(team, name, id = '0')
+  dir = File.join('../stats', team)
+  FileUtils.mkdir_p dir
+  File.join(dir, name + '---' + id)
+end
+
 people_ids.each do |team, name, id|
   puts "Loading id #{name} / #{team} / #{id}"
-  dir = File.join('../stats', team)
-
-  file = File.join(dir, name + '---' + id)
+  file = get_stat_path(team, name, id)
   next if File.exist? file
 
   page = load_page(id)
@@ -52,6 +64,11 @@ people_ids.each do |team, name, id|
   flag = page.at_css('.highcharts-subtitle').text
   data[:score] = parse_score(page) unless flag =~ /NO RECORDS/
 
-  FileUtils.mkdir_p dir
   File.open(file, 'w') { |fh| fh << data.to_json }
+end
+
+wildcards.each do |team, name|
+  puts "Creating file for wildcard #{name} / #{team}"
+  data = { name: name, team: team }
+  File.open(get_stat_path(team, name), 'w') { |fh| fh << data.to_json }
 end
